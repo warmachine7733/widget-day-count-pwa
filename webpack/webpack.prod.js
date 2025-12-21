4ffab88e0f1b4ba43e03bfe4c89044e71d0176a0
prateek <prateek@prateeks-MacBook-Air.local>
2025-12-21 21:40:33 +0530

    fix: import path

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,7 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const path = require('path');
 
 module.exports = merge(common, {
   entry: "./src/index.tsx",
