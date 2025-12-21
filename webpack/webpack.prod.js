7b306cee08e4c8c83658151dcbe31952ed98a352
prateek <prateek@prateeks-MacBook-Air.local>
2025-12-21 21:52:03 +0530

    prod webpack fix

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,16 +1,13 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const path = require('path');
 
 module.exports = merge(common, {
   entry: "./src/index.tsx",
   mode: "production",
   output: {
-    path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "main.[contenthash].js",
-    clean: true
+    filename: "main.js",
   },
   plugins: [new CompressionPlugin()],
 });
